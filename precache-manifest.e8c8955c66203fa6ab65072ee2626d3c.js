cded2630646b89ca1c08a81924526023480a51f2
Carleen Thio <carleen.thio@gmail.com>
2019-12-06 18:57:10 -0500

    Updates

---
+++ precache-manifest.e8c8955c66203fa6ab65072ee2626d3c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be931d626bba7e94e67bd0c12ddd8d57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "955738486558d240fba7",
+    "url": "/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c7db3c12f58c5d738366",
+    "url": "/static/css/main.4b0740d9.chunk.css"
+  },
+  {
+    "revision": "955738486558d240fba7",
+    "url": "/static/js/2.48dce8a5.chunk.js"
+  },
+  {
+    "revision": "c7db3c12f58c5d738366",
+    "url": "/static/js/main.eff86685.chunk.js"
+  },
+  {
+    "revision": "6da5b77aa5ea0e08734c",
+    "url": "/static/js/runtime-main.300ee560.js"
+  },
+  {
+    "revision": "2b8ba67d8ababf49da26440373396a44",
+    "url": "/static/media/Resume.2b8ba67d.pdf"
+  },
+  {
+    "revision": "77efbcd3f76d0d3b006f4d496da75662",
+    "url": "/static/media/axure.77efbcd3.jpg"
+  },
+  {
+    "revision": "4026100f8becdae1b99cf159fe880f29",
+    "url": "/static/media/birdie.4026100f.jpg"
+  },
+  {
+    "revision": "c621a87f2942827466c0c41da9cbbdbb",
+    "url": "/static/media/brick-breaker.c621a87f.jpg"
+  },
+  {
+    "revision": "54760b60a80a02c512d4419fddcfeae5",
+    "url": "/static/media/design-process.54760b60.png"
+  },
+  {
+    "revision": "d801087d1f25556d28229da358a72df2",
+    "url": "/static/media/diamond-design.d801087d.png"
+  },
+  {
+    "revision": "a1dff24c2cbcd4d35873c5f3e4ae287a",
+    "url": "/static/media/f1.a1dff24c.jpg"
+  },
+  {
+    "revision": "eec5e823d77806580a64e8e2024b676a",
+    "url": "/static/media/final-screen.eec5e823.png"
+  },
+  {
+    "revision": "882aef9729e30bbf2e900aa2131af99c",
+    "url": "/static/media/fudger-additionals.882aef97.png"
+  },
+  {
+    "revision": "1f9429a45a75c13baa34479e0aa1462f",
+    "url": "/static/media/fudger-grocery.1f9429a4.png"
+  },
+  {
+    "revision": "91ed6275f45461fe124d612d15f1670b",
+    "url": "/static/media/fudger-inventory.91ed6275.png"
+  },
+  {
+    "revision": "dccbdbcf4070f6810789b7fce1de89ab",
+    "url": "/static/media/homepage.dccbdbcf.png"
+  },
+  {
+    "revision": "17e5e29072fb5745dc4eacef542f00f0",
+    "url": "/static/media/jammy.17e5e290.JPG"
+  },
+  {
+    "revision": "b75d4f50504e105b45b9b2a54998ed16",
+    "url": "/static/media/lang-result.b75d4f50.jpg"
+  },
+  {
+    "revision": "a459362a9d42541cb6fed34473215d6e",
+    "url": "/static/media/language.a459362a.jpg"
+  },
+  {
+    "revision": "fadeeb59183f1c6b925adc26ac38190c",
+    "url": "/static/media/p1.fadeeb59.JPG"
+  },
+  {
+    "revision": "1ee56db70f721b20bc3f64b5f256ce55",
+    "url": "/static/media/p10.1ee56db7.JPG"
+  },
+  {
+    "revision": "3dc4b209e720726007d3834954b2e2f1",
+    "url": "/static/media/p2.3dc4b209.jpg"
+  },
+  {
+    "revision": "b27eaf94de5fb99476be527dea89adc6",
+    "url": "/static/media/p3.b27eaf94.JPG"
+  },
+  {
+    "revision": "bd5aa0d9478f7c665bae5188c843b329",
+    "url": "/static/media/p4.bd5aa0d9.jpg"
+  },
+  {
+    "revision": "e3e81f4dd1c7142bbbba8ebaef15c47e",
+    "url": "/static/media/p5.e3e81f4d.JPG"
+  },
+  {
+    "revision": "dbe684a7f81226e24bd2aefc3bb1b761",
+    "url": "/static/media/p6.dbe684a7.JPG"
+  },
+  {
+    "revision": "3d9aa362bc6560ab7a0457cbf298d7c3",
+    "url": "/static/media/p7.3d9aa362.JPG"
+  },
+  {
+    "revision": "fd713b911360a15a42b1fca3f7bfd25c",
+    "url": "/static/media/p8.fd713b91.JPG"
+  },
+  {
+    "revision": "8192bb791a43df45668127e766fd9553",
+    "url": "/static/media/p9.8192bb79.JPG"
+  },
+  {
+    "revision": "c93d91bbaf71e2ede89af50dba8d29af",
+    "url": "/static/media/paper-add.c93d91bb.JPG"
+  },
+  {
+    "revision": "8204ffd22b781ec32acda678c8bb5bf3",
+    "url": "/static/media/paper-grocery.8204ffd2.JPG"
+  },
+  {
+    "revision": "00725417a21ae076f08cbf3b22a5b82b",
+    "url": "/static/media/paper-inventory.00725417.JPG"
+  },
+  {
+    "revision": "33dbfb58b9b332add950067fe9815db5",
+    "url": "/static/media/valkyrie.33dbfb58.jpg"
+  }
+]);
