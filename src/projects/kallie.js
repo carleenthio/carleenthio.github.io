c70bdb28d7865c3138c56019d4aee258a5ed3727
Carleen Thio <carleen.thio@gmail.com>
2020-02-20 10:36:26 -0500

    Adding two new projects
    Kalli and ux writing added. writing not published though

---
+++ src/projects/kallie.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import '../css/projects.css'
+import { Table } from 'react-bootstrap'
+import { LinkContainer } from "react-router-bootstrap";
+import Breadcrumb from 'react-bootstrap/Breadcrumb'
+import kallietemp from '../pictures/kallie/kallie-overview-jan.png'
+class Kallie extends React.Component {    
+	render() {
+		return (
+			<div className='kallie'>
+                <Breadcrumb>
+                    <LinkContainer to="/projects">
+                        <Breadcrumb.Item>Projects</Breadcrumb.Item>
+                    </LinkContainer>
+                    <Breadcrumb.Item active>Kallie</Breadcrumb.Item>
+                </Breadcrumb>
+                
+                {/* page content */}
+                <Container>
+                    <h1>[WIP] Kallie: Reinventing Calendars</h1>
+                    <br></br>
+                    <h2>Project Snapshot</h2>
+                    
+                    <div className="row">
+                        <div className="column"><h4>The Problem</h4>
+                        <p>
+                            I noticed a problem in the plant and trading communnity in Toronto.
+                            Both people who had few or lots of trades had trouble keeping up with date trades.
+                            Even if they had alternatives like pen and paper or their ohone calendars.
+                        </p>
+                        
+                        <h4>Objective</h4>
+                        <p>
+                            To identify a root cause for the failure of the current 
+                            available scheduling system and design an alternate solution 
+                            that could help mitigate food waste. 
+                        </p>
+                        <p>
+                            Side note: The project was also started to brush up on the UX process
+                        </p>
+                        
+                        <h4>The Solution</h4>
+                        <p>
+                            A mobile application with more specific inputs to help relief users' 
+                            cognitive load that allows integration with their mobile's native
+                            calendar.
+                        </p>
+
+                        <h4>Tools Used</h4>
+                        <p>Adobe XD</p>
+
+                        <h4>Project Length</h4>
+                        <p>2.5 months</p>
+                        <h4>My Role</h4>
+                        <p>
+                            Lead UX Researcher and Designer
+                        </p>
+                        <p>
+                        Guided team member through the UX research and design process
+                        </p>
+                        <img className='kallie-project' alt="overview of Kalli in January" src={kallietemp} width="100%"/>   
+                    
+                        </div>
+                    {/* <div className="column2">  
+                    </div> */}
+                    </div>
+                    {/* <h4>Scope and Constraints</h4>
+                    <p>
+                        While we tried to allow it to be used for other
+                    </p> */}
+                    {/* </div> */}
+                    <h3> Discovery</h3>
+                    <p> What was conducted</p>
+                    <ul>
+                        <li>User surveys in trading groups and plant communities</li>
+                        <li>Heursitic </li>
+                    </ul>
+                    <hr/>
+                    <h3> Define </h3>
+                    <p> What was conducted</p>
+                    <ul>
+                        <li>User flows</li>
+                        <li>Personas </li>
+                        <li>Information Architecture</li>
+                    </ul>
+                    <hr/>
+                    <h3> Design </h3>
+                    {/* <Design/> */}
+                    <hr/>
+                    <h3>Develop</h3>
+                    </Container>
+			</div>
+		);
+	}
+}
+export default Kallie
--- src/projects/ux-writing.js
+++ src/projects/ux-writing.js
@@ -31,13 +31,24 @@ class UxWriting extends React.Component {
                         Button(s): 25 characters max
                     </p>
                     <p>
-                    Headline:
-                    Notice: Flight Cancelled Due to Bad Weather [43 character]
-                    Body & Button 1:
-                    Thank you for flying with us. Sadly we have to cancel your flight due to bad weather. 
-                    But don't worry! Our team will be able to assist and find you another flight! [163 characters]
+                        Notice: Flight Cancelled Due to Bad Weather [43 character]
+                    
+                        Thank you for flying with us. Sadly we have to cancel your flight due to bad weather.
+                        But don't worry! We found a few flights that will take you to your destination. 
+                        [167 characters]
 
-                    Find my next flight [19 character]
+                        Book my next flight [19 character]
+                    </p>
+                </span>
+                <span>
+                    <h3>UX Writing Challenge: Day 2</h3>
+                    <p>Scenario: A user is a working parent, and a big sports fan, in the midst of their favorite sports season who can no longer attend games.
+
+                    Challenge: Write a promotional screen for an app that lets a user choose teams, sends game reminders, real-time score updates and highlight videos.
+                    
+                    Headline: 40 characters max
+                    Body: 175 characters max
+                    Button(s): 25 characters max
                     </p>
                 </span>
                 </Container>
