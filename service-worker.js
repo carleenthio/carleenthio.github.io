ebabcb260c59769c508a4fb98c1b83f591161163
Carleen Thio <carleen.thio@gmail.com>
2020-02-20 11:51:35 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f61aeb006a555b7ffb5540e6dedbab3.js"
+  "/precache-manifest.80a612e936cb3d06520c968ddfe782d5.js"
 );
 
 self.addEventListener('message', (event) => {
